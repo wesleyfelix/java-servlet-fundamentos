c19492da3d32b04d09825f855753afb715af4b1a
wesleyfelix <wesley.felixdelima96@gmail.com>
2021-12-30 14:07:11 -0300

    iniciado o crud, delete

--- src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,19 +1,23 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	private static Integer chaveSequencial = 1;
 
 	
 	static {
-		Empresa empresa = new Empresa();
-		empresa.setNome("alura");
 		
+		Empresa empresa = new Empresa();
+		empresa.setId(Banco.chaveSequencial++);
+		empresa.setNome("Alura");
 		
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(Banco.chaveSequencial++);
 		empresa2.setNome("Caelum");
 		
 		lista.add(empresa);
@@ -22,13 +26,26 @@ public class Banco {
 	}
 	
 	public void adiciona(Empresa empresa) {
-		
+		empresa.setId(Banco.chaveSequencial++);
 		lista.add(empresa);
 		
 	}
 	
 	public List<Empresa> getEmpresas(){
 		return Banco.lista;
+	}
+
+	public void removeEmpresa(Integer id) {
+		
+		Iterator<Empresa> it = lista.iterator();
+		while(it.hasNext()) {
+			Empresa emp = it.next();		
+			if(emp.getId() == id) {
+				it.remove();;
+			}
+		
+		}
+		
 	}
 
 }
