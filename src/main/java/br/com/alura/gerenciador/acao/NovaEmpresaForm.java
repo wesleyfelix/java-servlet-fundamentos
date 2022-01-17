6b85a49b83a202f52f56ab36ae62d541da9cb1e6
wesleyfelix <wesley.felixdelima96@gmail.com>
2022-01-17 16:05:46 -0300

    ajustando a pasta correta dos JSP

---
+++ src/main/java/br/com/alura/gerenciador/acao/NovaEmpresaForm.java
@@ -0,0 +1,30 @@
+package br.com.alura.gerenciador.acao;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
+
+public class NovaEmpresaForm {
+	
+	public String executa(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {
+		
+		
+System.out.println("Cadastrando nova empresa");
+		
+	
+		return "forward:formNovaEmpresa.jsp";
+
+		
+	}
+}
