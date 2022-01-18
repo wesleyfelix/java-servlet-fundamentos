b9af096ada2ca1b737715b5ea423700e5070b286
wesleyfelix <wesley.felixdelima96@gmail.com>
2022-01-18 16:43:52 -0300

    criação e verificação de logi ne senha

--- src/main/java/br/com/alura/gerenciador/modelo/Banco.java
+++ src/main/java/br/com/alura/gerenciador/modelo/Banco.java
@@ -7,6 +7,8 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	private static List<Usuario> listaUsuarios = new ArrayList<Usuario>();
+	
 	private static Integer chaveSequencial = 1;
 
 	
@@ -22,6 +24,18 @@ public class Banco {
 		
 		lista.add(empresa);
 		lista.add(empresa2);
+		
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("wesley");
+		u1.setSenha("1234");
+		
+		Usuario u2 = new Usuario();
+		u2.setLogin("felix");
+		u2.setSenha("1234");
+		
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
 		
 	}
 	
@@ -55,6 +69,15 @@ public class Banco {
 			}
 		}
 		
+		return null;
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		for(Usuario usuario : listaUsuarios) {
+			if(usuario.ehIgual(login, senha)) {
+				return usuario;
+			}
+		}
 		return null;
 	}
 
