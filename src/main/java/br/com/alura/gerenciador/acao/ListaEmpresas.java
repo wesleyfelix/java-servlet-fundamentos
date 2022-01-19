eeba819dae8cc656edad78c0ddd3164778a6a7e3
wesleyfelix <wesley.felixdelima96@gmail.com>
2022-01-19 15:47:29 -0300

    adicionando filtro no projeto

--- src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -17,12 +17,6 @@ public class ListaEmpresas implements Acao{
 	public String executa(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		
-		HttpSession sessao = request.getSession();
-		
-		if(sessao.getAttribute("usuarioLogado") == null) {
-			return "redirect:entrada?acao=LoginForm";
-		}
-		
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
 		
---
+++ src/main/java/br/com/alura/gerenciador/servlet/MonitoramentoFilter.java
@@ -0,0 +1,34 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/entrada")
+public class MonitoramentoFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		long antes = System.currentTimeMillis();
+		
+		
+		
+		//executa a ação
+		
+		chain.doFilter(request, response);
+		
+		
+		String acao = request.getParameter("acao");
+		
+		long depois = System.currentTimeMillis();
+		System.out.println("tempo de execução da ação "+ acao +": " + (depois - antes) + " milisegundos");
+	}
+
+	
+}
